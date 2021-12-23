976b83bd1a53b8a1f1a13a8959979330e89a21fa
WhyK <windchimeyk@gmail.com>
2021-12-23 12:38:43 +0900

    style: deno fmt

--- test/e2e/mod.test.ts
+++ test/e2e/mod.test.ts
@@ -15,7 +15,7 @@ const launchSinco = async (
   return { sinco, page };
 };
 
-const BASE_URL = "https://unrewrite-sample.deno.dev"
+const BASE_URL = "https://unrewrite-sample.deno.dev";
 
 Deno.test("Go to 404 page", async () => {
   const { sinco, page } = await launchSinco({
@@ -28,37 +28,37 @@ Deno.test("Go to 404 page", async () => {
   await sinco.done();
 });
 
-Deno.test("Go to jpg url",async () => {
-  const JPG_PATH = "images/jpgsample.jpg"
+Deno.test("Go to jpg url", async () => {
+  const JPG_PATH = "images/jpgsample.jpg";
   const { sinco, page } = await launchSinco({
     url: `${BASE_URL}/${JPG_PATH}`,
   });
   const imgSrc: string = await page.evaluate(() => {
-    return document.querySelector("img")?.src
+    return document.querySelector("img")?.src;
   });
   assertStringIncludes(imgSrc, JPG_PATH);
   await sinco.done();
-})
+});
 
-Deno.test("Go to png url",async () => {
-  const PNG_PATH = "images/pngsample.png"
+Deno.test("Go to png url", async () => {
+  const PNG_PATH = "images/pngsample.png";
   const { sinco, page } = await launchSinco({
     url: `${BASE_URL}/${PNG_PATH}`,
   });
   const imgSrc: string = await page.evaluate(() => {
-    return document.querySelector("img")?.src
+    return document.querySelector("img")?.src;
   });
   assertStringIncludes(imgSrc, PNG_PATH);
   await sinco.done();
-})
+});
 
-Deno.test("Go to svg url",async () => {
+Deno.test("Go to svg url", async () => {
   const { sinco, page } = await launchSinco({
     url: `${BASE_URL}/images/svgsample.svg`,
   });
   const svgId: string = await page.evaluate(() => {
-    return document.querySelector("rect")?.id
+    return document.querySelector("rect")?.id;
   });
   assertEquals(svgId, "アートボード1");
   await sinco.done();
-})
+});
